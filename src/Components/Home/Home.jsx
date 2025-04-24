19ed3542cbdd10cc02e5f83d69566d2e887f264f
Santosh <santoshparida891@gmail.com>
2025-04-24 20:34:11 +0545

    CV

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         </p>
         <a 
           href={cvFile} // Link to your CV file
-          download="My_CV.pdf" // Name the file when downloaded
+          download="cv.pdf" // Name the file when downloaded
         >
           <button className='mt-5 md:md-10 cursor-pointer text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>
             Download CV
